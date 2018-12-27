e4a47500f85fcab5b37e68baa153fe7a00323347
StolarQQ <stolar28@gmail.com>
2018-12-27 14:11:23 +0100

    Added interaction between DailyRoute and PassengerNode

--- Passenger.Core/Domain/PassengerNode.cs
+++ Passenger.Core/Domain/PassengerNode.cs
@@ -4,5 +4,20 @@
     {
         public Node Node { get; protected set; }
         public Passenger Passenger { get; protected set; }
+
+        // Constructor for serialization
+        protected PassengerNode()
+        {
+            
+        }
+
+        public PassengerNode(Passenger passenger, Node node)
+        {
+            Passenger = passenger;
+            Node = node;
+        }
+
+        public static PassengerNode Create(Passenger passenger, Node node)
+            => new PassengerNode(passenger, node);
     }
 }
